6890a6898e5437a4f4e523707566cba4427fe4d1
Jitendra <jk2praj@gmail.com>
2024-07-20 22:03:08 +0800

    Created H2 DB

--- src/main/kotlin/com/hoppers/towntalk/beans/User.kt
+++ src/main/kotlin/com/hoppers/towntalk/beans/User.kt
@@ -4,11 +4,15 @@ import jakarta.annotation.Generated
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 import java.util.*
 
 
 @Entity
-@Table(name = "app_user")
+@Table(
+    name = "app_user",
+    uniqueConstraints = [UniqueConstraint(columnNames = arrayOf("email"))]
+)
 data class User(
     @Id
     @Generated
